aebaba325829e740bf6e665e2e5d1e79cef99ece
mohamed <khaldimohamed82@gmail.com>
2025-10-18 20:15:05 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- philo.c
+++ philo.c
@@ -13,6 +13,7 @@ int ft_simulation_stop(t_philo *philo) {
 /*print status of philo function*/
 void ft_print_status(t_philo *philo, char *status) {
   long long timestamp;
+
   if (philo->table->simulation_stop) {
     return;
   }
@@ -20,13 +21,44 @@ void ft_print_status(t_philo *philo, char *status) {
   printf(" %lld %d %s", timestamp, philo->id, status);
 }
 
+/*take fork function*/
+void ft_philo_take_fork(t_philo *philo) {
+  if (philo->id % 2 == 0) {
+    ft_print_status(philo, "philo taken right fork");
+    ft_print_status(philo, "philo taken leftchar *status fork");
+  } else {
+    ft_print_status(philo, "philo taken left fork");
+    ft_print_status(philo, "philo taken right fork");
+  }
+}
+
+/*eating function*/
+void ft_philo_eat(t_philo *philo) {
+  ft_print_status(philo, "is eating");
+  philo->last_meal_time = ft_get_current_time();
+  philo->meals_eaten++;
+  usleep(philo->table->time_to_eat * 1000);
+}
+
 /*think function*/
 void ft_philo_think(t_philo *philo) {
   ft_print_status(philo, "Philo is thinking");
 }
 
-/*take fork function*/
-void ft_philo_take_fork(t_philo *philo) { ft_print_status(philo, "") }
+/*puts the fork func*/
+void ft_philo_puts_fork(t_philo *philo) {
+  if (philo->id % 2 == 0) {
+    /* TODO:  <18-10-25, unlock mutex> */
+  } else {
+    /* TODO:  <18-10-25,unlock mutex> */
+  }
+}
+
+/*sleep function*/
+void ft_philo_sleep(t_philo *philo) {
+  ft_print_status(philo, "is sleep");
+  usleep(philo->table->time_to_die * 1000);
+}
 
 /*routin function*/
 void *ft_routine(void *arg) {
