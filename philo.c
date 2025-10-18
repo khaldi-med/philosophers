63962e435108b0a6b06a4cac87312e45a029f981
mohamed <khaldimohamed82@gmail.com>
2025-10-18 21:50:52 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- philo.c
+++ philo.c
@@ -1,6 +1,5 @@
 #include "philo.h"
-#include <stdio.h>
-#include <time.h>
+#include <pthread.h>
 
 /*check the flag for simulation stats*/
 int ft_simulation_stop(t_philo *philo) {
@@ -18,23 +17,31 @@ void ft_print_status(t_philo *philo, char *status) {
     return;
   }
   timestamp = ft_get_current_time() - philo->table->start_time;
-  printf(" %lld %d %s", timestamp, philo->id, status);
+  printf(" %lld %d %s\n", timestamp, philo->id, status);
 }
 
 /*take fork function*/
 void ft_philo_take_fork(t_philo *philo) {
   if (philo->id % 2 == 0) {
-    ft_print_status(philo, "philo taken right fork");
-    ft_print_status(philo, "philo taken leftchar *status fork");
+    pthread_mutex_lock(&philo->right_f->mutex);
+    ft_print_status(philo, "philo taken right fork\n");
+    pthread_mutex_unlock(&philo->right_f->mutex);
+    pthread_mutex_lock(&philo->left_f->mutex);
+    ft_print_status(philo, "philo taken left fork\n");
+    pthread_mutex_unlock(&philo->left_f->mutex);
   } else {
-    ft_print_status(philo, "philo taken left fork");
-    ft_print_status(philo, "philo taken right fork");
+    pthread_mutex_lock(&philo->left_f->mutex);
+    ft_print_status(philo, "philo taken left fork\n");
+    pthread_mutex_unlock(&philo->left_f->mutex);
+    pthread_mutex_lock(&philo->right_f->mutex);
+    ft_print_status(philo, "philo taken right fork\n");
+    pthread_mutex_unlock(&philo->right_f->mutex);
   }
 }
 
 /*eating function*/
 void ft_philo_eat(t_philo *philo) {
-  ft_print_status(philo, "is eating");
+  ft_print_status(philo, "is eating\n");
   philo->last_meal_time = ft_get_current_time();
   philo->meals_eaten++;
   usleep(philo->table->time_to_eat * 1000);
@@ -42,7 +49,7 @@ void ft_philo_eat(t_philo *philo) {
 
 /*think function*/
 void ft_philo_think(t_philo *philo) {
-  ft_print_status(philo, "Philo is thinking");
+  ft_print_status(philo, "Philo is thinking\n");
 }
 
 /*puts the fork func*/
@@ -56,7 +63,7 @@ void ft_philo_puts_fork(t_philo *philo) {
 
 /*sleep function*/
 void ft_philo_sleep(t_philo *philo) {
-  ft_print_status(philo, "is sleep");
+  ft_print_status(philo, "is sleep\n");
   usleep(philo->table->time_to_die * 1000);
 }
 
@@ -66,14 +73,13 @@ void *ft_routine(void *arg) {
 
   philo = (t_philo *)arg;
   if (philo->table->num_philos == 1) {
-    ft_print_status(philo, "has take a fork");
+    ft_print_status(philo, "has take a fork\n");
     usleep(philo->table->time_to_die * 1000);
     return (NULL);
   }
   if (philo->id % 2 == 0)
     usleep(15000);
   while (!ft_simulation_stop(philo)) {
-    /* TODO:  <18-10-25, creat_funs> */
     ft_philo_think(philo);
     ft_philo_take_fork(philo);
     ft_philo_eat(philo);
@@ -81,6 +87,45 @@ void *ft_routine(void *arg) {
     ft_philo_sleep(philo);
   }
   return (NULL);
+}
+
+void ft_init_philos(t_philo *philos, t_table *table) {
+  int i;
+  int j;
+
+  i = 0;
+  while (i < table->num_philos) {
+    table->forks[i].id = i;
+    i++;
+  }
+  j = 0;
+  while (j < table->num_philos) {
+    philos[j].id = j + 1;
+    philos[j].meals_eaten = 0;
+    philos[j].table = table;
+    philos[j].left_f = &table->forks[j];
+    philos[j].right_f = &table->forks[(j + 1) % table->num_philos];
+    j++;
+  }
+  table->start_time = ft_get_current_time();
+  table->simulation_stop = 0;
+}
+
+int ft_init_philo_thread(t_philo *philos, t_table *table) {
+  int i;
+
+  i = 0;
+  while (i < table->num_philos) {
+    if (pthread_create(&philos[i].thread, NULL, ft_routine, &philos[i]) != 0) {
+      // Handle thread creation error
+      printf("Error: Failed to create thread\n");
+      return (1);
+    }
+    i++;
+  }
+  usleep(500000);
+  table->simulation_stop = 0;
+  return 0;
 }
 
 /*main  function*/
@@ -92,6 +137,7 @@ int main(int ac, char **av) {
     printf("arguments not valid");
     return (0);
   }
+
   table.num_philos = ft_atoi(av[1]);
   table.time_to_die = ft_atoi(av[2]);
   table.time_to_eat = ft_atoi(av[3]);
@@ -100,27 +146,8 @@ int main(int ac, char **av) {
   if (ac == 6)
     table.num_eat_count = ft_atoi(av[5]);
   table.forks = malloc(sizeof(t_fork) * table.num_philos);
-  for (int i = 0; i < table.num_philos; i++) {
-    table.forks[i].id = i;
-  }
   philos = malloc(sizeof(t_philo) * table.num_philos);
-  for (int i = 0; i < table.num_philos; i++) {
-    philos[i].id = i + 1;
-    philos[i].meals_eaten = 0;
-    philos[i].table = &table;
-    philos[i].left_f = &table.forks[i];
-    philos[i].right_f = &table.forks[(i + 1) % table.num_philos];
-    /* printf("philo num: %d\n", philos[i].id); */
-    /* printf("philo num meals_eaten: %d\n", philos[i].meals_eaten); */
-    /* printf("philo %d has fork %d and %d\n", philos[i].id,
-     * philos[i].left_f->id, */
-    /* philos[i].right_f->id); */
-  }
-  table.start_time = ft_get_current_time();
-  table.simulation_stop = 0;
-  /* printf("the current time in milliscond: %lld\n", table.start_time); */
-  for (int i = 0; i < table.num_philos; i++) {
-    /* pthread_create(&philos[i].thread, NULL, routine, &philos[i]); */
-  }
+  ft_init_philos(philos, &table);
+  ft_init_philo_thread(philos, &table);
   return (0);
 }
